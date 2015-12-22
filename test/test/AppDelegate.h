10b63521bee20d87c3686abacdbabe99e39cb49c
yick <yick8799@163.com>
2015-12-22 15:19:52 +0800

    去除storyboard

--- test/test/AppDelegate.h
+++ test/test/AppDelegate.h
@@ -8,10 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (nonatomic, strong) ViewController *viewController;
 
 @end
 
--- test/test/AppDelegate.m
+++ test/test/AppDelegate.m
@@ -17,6 +17,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    self.viewController = [[ViewController alloc] init];
+    
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
